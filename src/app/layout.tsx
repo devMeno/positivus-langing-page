138b6bb5b3647cb64fa68bb91cb1dbd7214f88cf
Charles Amen <charlesamenlokonon@gmail.com>
2025-03-06 07:10:40 +0100

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const spaceGrotesk = Space_Grotesk({subsets:["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${spaceGrotesk.className}`}
       >
-        {children}
+        <div className={"@container bg-red-500 px-[20px] md:px-[35px] lg:px-[70px]"}>
+            {children}
+        </div>
       </body>
     </html>
   );
